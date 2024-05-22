1579876ecf85e14d0412ebd5e82360c61289d37b
hussaindev99 <naini.dev0@gamil.com>
2024-05-22 10:06:53 -0700

    multipage

---
+++ chat.js
@@ -0,0 +1,22 @@
+function sendMessage() {
+    var messageInput = document.getElementById("messageInput");
+    var message = messageInput.value.trim();
+
+    if (message !== "") {
+        var chatContainer = document.getElementById("chatContainer");
+        var sentMessage = document.createElement("div");
+        sentMessage.className = "chat-message sent enter";
+        sentMessage.textContent = message;
+        chatContainer.appendChild(sentMessage);
+        setTimeout(function () {
+            var receivedMessage = document.createElement("div");
+            receivedMessage.className = "chat-message received";
+            receivedMessage.textContent = "Hello ! Nazrul How are you";
+            chatContainer.appendChild(receivedMessage);
+
+            chatContainer.scrollTop = chatContainer.scrollHeight;
+        }, 500);
+        messageInput.value = "";
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+    }
+}
